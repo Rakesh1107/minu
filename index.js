ca1db5bc663a471a34f424bf036d69d3d1f327db
Ragesh M <mragesh1998@gmail.com>
2021-07-05 17:42:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+// Mouse click & Mobile tap
 $(".section").on("click tap", function() {
 	var alphabet = $(this).attr("id");
-	var audio = new Audio("sounds/" + alphabet + ".mp3");
-	audio.play();
+	var clickAudio = new Audio("sounds/" + alphabet + ".mp3");
+	playAudio(clickAudio);
 });
+
+// Keyboard Press
+document.addEventListener("keydown", function(e) {
+  var keyPressed = e.key;
+  var keyAudio = new Audio("sounds/" + keyPressed + ".mp3");
+  playAudio(keyAudio);
+  animate(keyPressed);
+});
+
+//Play audio 
+function playAudio(audio) {
+	audio.play();
+}
+
+// function animate(button) {
+// 	$("#" + button).addClass("animate");
+//   setTimeout(function() {
+//   	$("#" + button).removeClass("animate")
+//   }, 200);
+// }
